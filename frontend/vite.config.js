182a86b91364b1115260bba350aa15e8cf52381e
Ritik Sachan <sachanritik1@gmail.com>
2024-01-26 19:13:43 +0530

    Add dotenv package and update API_URL constant

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+import dotenv from "dotenv";
+dotenv.config();
+
+//https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_BASE_URL,
+        changeOrigin: true,
+      },
+    },
+  },
 });
